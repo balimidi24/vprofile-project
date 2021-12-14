f38704c12920572ab5edcec21a990a36ff433917
Ganesh Balimidi <balimidi.ganesh@outlook.com>
2021-12-15 00:08:16 +0530

    new update

---
+++ vagrant/Manual_provisioning/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"db1e6d06ab24fcf3ce96b72c46afa3f751e473968a87b1b9c9ecaa8eb1bb329c","vagrant_version":"2.2.19"}
